ce273790d7078869972efe31cf3095047d6775ce
Tranduong03 <106472477+Tranduong03@users.noreply.github.com>
2024-12-25 02:23:17 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using dethithu.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dethithu.Controllers
+{
+    public class HomeController : Controller
+    {
+        QuanLyBanQuanAoEntities data = new QuanLyBanQuanAoEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var allProducts = data.Sanphams.ToList();
+            ViewData["AllProducts"] = allProducts;
+            return View();
+        }
+
+        public ActionResult Phanloai() 
+        { 
+            var phanloai = from pl in data.PhanLoaiSanPhams select pl;
+            return PartialView(phanloai);
+        }
+
+        public ActionResult SanPhamTheoPhanLoai(int id) 
+        {
+            var Sanpham = id == 0 
+                ? data.Sanphams.ToList()
+                : data.Sanphams.Where(sp => sp.PhanLoaiSanPhamID == id).ToList();
+
+            //var Sanpham = from sp in data.Sanphams where sp.PhanLoaiSanPhamID == id select sp;
+
+            return PartialView(Sanpham);
+        }
+
+        public ActionResult ThemSanPham()
+        {
+            var phanloai = data.PhanLoaiSanPhams.ToList();
+            ViewBag.PhanLoaiSanPhamList = new SelectList(phanloai, "PhanLoaiSanPhamID", "TenPhanLoai");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ThemSanPham(Sanpham sanpham)
+        {
+            if (ModelState.IsValid)
+            {
+                if (sanpham.ImageFile != null && sanpham.ImageFile.ContentLength > 0)
+                {
+                    var allowedExtensions = new[] { ".jpg" };
+                    var fileExtension = Path.GetExtension(sanpham.ImageFile.FileName).ToLower();
+
+                    if (!allowedExtensions.Contains(fileExtension))
+                    {
+                        ModelState.AddModelError("ImageFile", "Only .jpg files are allowed.");
+                    }
+                    else
+                    {
+                        try
+                        {
+                            var fileName = Path.GetFileName(sanpham.ImageFile.FileName);
+                            var path = Path.Combine(Server.MapPath("~/Content/images/"), fileName);
+                            sanpham.ImageFile.SaveAs(path);
+                            sanpham.AnhDaiDien = fileName;
+                        }
+                        catch (Exception ex)
+                        {
+                            ModelState.AddModelError("", $"Error saving file: {ex.Message}");
+                        }
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    data.Sanphams.Add(sanpham); 
+                    data.SaveChanges();        
+
+                    return RedirectToAction("Index"); 
+                }
+            }
+
+            // Nếu có lỗi, trả về form cùng với danh sách phân loại sản phẩm
+            ViewBag.PhanLoaiSanPhamList = new SelectList(data.PhanLoaiSanPhams, "PhanLoaiSanPhamID", "TenPhanLoai");
+            return View(sanpham);
+        }
+
+    }
+}
---
+++ Models/SanphamMetadata.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace dethithu.Models
+{
+    [MetadataType(typeof(SanphamMetadata))]
+    public partial class Sanpham
+    {
+        [NotMapped]
+        public HttpPostedFileBase ImageFile { get; set; }
+    }
+
+    public class SanphamMetadata
+    {
+        [Display(Name = "Mã sản phẩm")]
+        public int SanphamID { get; set; }
+
+        [Display(Name = "Tên sản phẩm")]
+        [Required(ErrorMessage = "Tên sản phẩm không được bỏ trống")]
+        public string TenSanpham { get; set; }
+
+        [Display(Name = "Mô tả")]
+        public string MoTa { get; set; }
+
+        [Display(Name = "Giá bán")]
+        [Required(ErrorMessage = "Giá bán không được bỏ trống")]
+        [Range(0, double.MaxValue, ErrorMessage = "Giá bán phải là số dương")]
+        [RegularExpression(@"^\d+(\.\d{1,2})?$")]
+        public decimal Gia { get; set; }
+
+        [Display(Name = "Trạng thái sản phẩm")]
+        public Nullable<byte> TrangThai { get; set; }
+
+        [Display(Name = "Ảnh đại diện cho sản phẩm")]
+        public string AnhDaiDien { get; set; }
+
+        [Display(Name = "Nổi bật")]
+        public Nullable<byte> NoiBat { get; set; }
+
+        [Display(Name = "Phân loại sản phảm")]
+        public Nullable<int> PhanLoaiSanPhamID { get; set; }
+
+    }
+}
